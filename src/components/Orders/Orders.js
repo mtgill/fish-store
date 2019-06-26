587cbca64c9518df08baa5f39d17fa7dc428a206
Matt Gill <mtgill90@outlook.com>
2019-06-25 21:23:52 -0500

    wip

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import ordersData from '../../helpers/data/ordersData';
+
+import OrderRow from '../OrderRow/OrderRow';
 import './Orders.scss';
 
 class Orders extends React.Component {
@@ -17,9 +19,27 @@ class Orders extends React.Component {
   }
 
   render() {
+    const orderComponents = this.state.orders.map(order => (
+      <OrderRow key={order.id} order={order} />
+    ));
     return (
       <div className="Orders">
-          <h1>Orders</h1>
+      <div className="Orders">
+        <h2>Orders</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Order #</th>
+              <th scope="col">Date</th>
+              <th scope="col"># Fish</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderComponents}
+          </tbody>
+        </table>
+      </div>
       </div>
     );
   }
